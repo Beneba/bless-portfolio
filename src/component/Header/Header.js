00f1f0afc104ea2c29077a6796ff9c5f54b57d20
Beneba <afetsib@gmail.com>
2021-10-31 14:32:01 +0000

    header page commented

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,51 +1,51 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link,Switch } from "react-router-dom";
-// import "../../main"
+// import React from "react";
+// import { BrowserRouter as Router, Route, Link,Switch } from "react-router-dom";
+// // import "../../main"
 
-function Header() {
-  return (
-    <Router>
-      <div>
-        <header>
-          <div className="menu-btn">
-            <div className="btn-line"></div>
-            <div className="btn-line"></div>
-            <div className="btn-line"></div>
-          </div>
+// function Header() {
+//   return (
+//     <Router>
+//       <div>
+//         <header>
+//           <div className="menu-btn">
+//             <div className="btn-line"></div>
+//             <div className="btn-line"></div>
+//             <div className="btn-line"></div>
+//           </div>
 
-          <nav className="menu">
-            <div className="menu-branding">
-              <div className="portrait"></div>
-            </div>
-            <ul className="menu-nav">
-              <li className="nav-item current">
-              <Link to = "/Home" className="nav-link" >Home</Link>
+//           <nav className="menu">
+//             <div className="menu-branding">
+//               <div className="portrait"></div>
+//             </div>
+//             <ul className="menu-nav">
+//               <li className="nav-item current">
+//               <Link to = "/Home" className="nav-link" >Home</Link>
                 
-              </li>
-              <li className="nav-item">
-                <Link to = "/About" className="nav-link" >About Me</Link>
-              </li>
-              <li className="nav-item">
-              <Link to = "/Work" className="nav-link" > My Work</Link>
+//               </li>
+//               <li className="nav-item">
+//                 <Link to = "/About" className="nav-link" >About Me</Link>
+//               </li>
+//               <li className="nav-item">
+//               <Link to = "/Work" className="nav-link" > My Work</Link>
                
-              </li>
-              <li className="nav-item">
-              <Link to = "/Contact" className="nav-link" >How To Reach Me</Link>
+//               </li>
+//               <li className="nav-item">
+//               <Link to = "/Contact" className="nav-link" >How To Reach Me</Link>
                
-              </li>
-            </ul>
-          </nav>
-        </header>
-      </div>
-      {/* <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/Home" component={Home} />
-          <Route path="/About" component={About} />
-          <Route path="/Work" component={Work} />
-          <Route path="/contact" component={Contact} />
-        </Switch> */}
-    </Router>
-  );
-}
+//               </li>
+//             </ul>
+//           </nav>
+//         </header>
+//       </div>
+//       {/* <Switch>
+//           <Route exact path="/" component={Home} />
+//           <Route exact path="/Home" component={Home} />
+//           <Route path="/About" component={About} />
+//           <Route path="/Work" component={Work} />
+//           <Route path="/contact" component={Contact} />
+//         </Switch> */}
+//     </Router>
+//   );
+// }
 
-export default Header;
+// export default Header;
